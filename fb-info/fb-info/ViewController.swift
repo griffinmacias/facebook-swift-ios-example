6d7d636495992ba7ad84f3396f7a62ff12e95e3a
griffinmacias <griffinmacias@gmail.com>
2017-08-30 22:33:47 -0500

    Adding FB delegate methods and grabbing of info

--- fb-info/fb-info/ViewController.swift
+++ fb-info/fb-info/ViewController.swift
@@ -8,14 +8,24 @@
 
 import UIKit
 import FacebookLogin
+import FacebookCore
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         let loginButton = LoginButton(readPermissions: [ .publicProfile, .email ])
+        loginButton.delegate = self
         loginButton.center = view.center
         
+        if let _ = AccessToken.current {
+            let request = GraphRequest(graphPath: "me",
+                                       parameters: [ "fields": "first_name, last_name, picture, email" ])
+            request.start { (response, result) in
+                print(result)
+            }
+        }
+
         view.addSubview(loginButton)
     }
 
@@ -23,7 +33,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension ViewController: LoginButtonDelegate {
+    
+    func loginButtonDidCompleteLogin(_ loginButton: LoginButton, result: LoginResult) {
+        switch result {
+        case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+            print(accessToken)
+            print(grantedPermissions)
+            print(declinedPermissions)
+        case .cancelled:
+            print("cancelled")
+        case .failed(let error):
+            print(error)
+        }
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: LoginButton) {
+        print(loginButton)
+    }
+}
