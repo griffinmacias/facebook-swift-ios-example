19601aeea39cc6b064a9e3e7ad3205293318f0cc
griffinmacias <griffinmacias@gmail.com>
2017-08-30 23:21:52 -0500

    Creating User model and working parsing dictionary

---
+++ fb-info/fb-info/Models.swift
@@ -0,0 +1,37 @@
+//
+//  Models.swift
+//  fb-info
+//
+//  Created by Mason Macias on 8/30/17.
+//  Copyright © 2017 griffinmacias. All rights reserved.
+//
+
+import Foundation
+import FacebookCore
+
+struct User {
+    var first: String?
+    var last: String?
+    var email: String?
+    var pictureUrl: String?
+    
+    init(_ dictionary: [String: Any]) {
+        
+        if let firstName = dictionary["first_name"] as? String {
+            self.first = firstName
+        }
+        if let lastName = dictionary["last_name"] as? String {
+            self.last = lastName
+        }
+        
+        if let email = dictionary["email"] as? String {
+            self.email = email
+        }
+        
+        if let picture = dictionary["picture"] as? [String: Any],
+        let data = picture["data"] as? [String: Any],
+            let picURL = data["url"] as? String {
+            self.pictureUrl = picURL
+        }
+    }
+}
--- fb-info/fb-info/ViewController.swift
+++ fb-info/fb-info/ViewController.swift
@@ -11,26 +11,38 @@ import FacebookLogin
 import FacebookCore
 
 class ViewController: UIViewController {
-
+    var loginButton: LoginButton?
     override func viewDidLoad() {
         super.viewDidLoad()
-        let loginButton = LoginButton(readPermissions: [ .publicProfile, .email ])
-        loginButton.delegate = self
-        loginButton.center = view.center
+        createFbLoginButton()
         if let _ = AccessToken.current {
-            let request = GraphRequest(graphPath: "me",
-                                       parameters: [ "fields": "first_name, last_name, picture, email" ])
-            request.start { (response, result) in
-                switch result {
-                case .failed(let error):
-                    print(error)
-                case .success(response: _):
-                    print(result)
+            getFbInfo()
+        }
+    }
+    
+    func createFbLoginButton() {
+        loginButton = LoginButton(readPermissions: [ .publicProfile, .email ])
+        if let loginButton = loginButton {
+            loginButton.delegate = self
+            loginButton.center = view.center
+            view.addSubview(loginButton)
+        }
+    }
+    
+    func getFbInfo() {
+        let request = GraphRequest(graphPath: "me",
+                                   parameters: [ "fields": "first_name, last_name, picture, email" ])
+        request.start { (response, result) in
+            switch result {
+            case .failed(let error):
+                print(error)
+            case .success(let graphResponse):
+                if let responseDictionary = graphResponse.dictionaryValue {
+                    print(responseDictionary)
+                    let user = User(responseDictionary)
                 }
             }
-        } 
-
-        view.addSubview(loginButton)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,6 +59,7 @@ extension ViewController: LoginButtonDelegate {
             print(accessToken)
             print(grantedPermissions)
             print(declinedPermissions)
+            getFbInfo()
         case .cancelled:
             print("cancelled")
         case .failed(let error):
